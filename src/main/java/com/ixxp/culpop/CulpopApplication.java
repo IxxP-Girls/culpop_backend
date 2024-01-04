24647454e77e9184c672ed7145b0737e0922cf15
diddnwjd <yang965273@gmail.com>
2024-01-05 00:12:08 +0900

    Fix: 빌드 테스트를 위해 작성한 코드 삭제

--- src/main/java/com/ixxp/culpop/CulpopApplication.java
+++ src/main/java/com/ixxp/culpop/CulpopApplication.java
@@ -3,7 +3,6 @@ package com.ixxp.culpop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
@@ -11,10 +10,5 @@ public class CulpopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CulpopApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String test() {
-		return "배포확인!!!";
 	}
 }
