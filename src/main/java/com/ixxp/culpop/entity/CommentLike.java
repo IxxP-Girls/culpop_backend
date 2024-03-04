d4523f0d4a8c9c91eb85c6db9cd6cb63df2dd194
diddnwjd <yang965273@gmail.com>
2024-03-04 22:04:17 +0900

    #23 Feat: 댓글 좋아요 기능 구현

--- src/main/java/com/ixxp/culpop/entity/CommentLike.java
+++ src/main/java/com/ixxp/culpop/entity/CommentLike.java
@@ -1,4 +1,26 @@
 package com.ixxp.culpop.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class CommentLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "commentId", nullable = false)
+    private Comment comment;
+
+    public CommentLike(User user, Comment comment) {
+        this.user = user;
+        this.comment = comment;
+    }
 }
---
+++ src/main/java/com/ixxp/culpop/mapper/CommentLikeMapper.java
@@ -0,0 +1,10 @@
+package com.ixxp.culpop.mapper;
+
+import com.ixxp.culpop.entity.CommentLike;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CommentLikeMapper {
+    void insertCommentLike(CommentLike commentLike);
+    boolean checkCommentLike(int userId, int commentId);
+}
