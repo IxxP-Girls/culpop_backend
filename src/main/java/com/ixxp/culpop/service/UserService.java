bdef5027b4d389a68f2a8970098c9fd17fa903aa
diddnwjd <yang965273@gmail.com>
2024-03-07 18:34:20 +0900

    Fix: 캐시 저장되도록 코드 수정

--- src/main/java/com/ixxp/culpop/service/UserService.java
+++ src/main/java/com/ixxp/culpop/service/UserService.java
@@ -75,6 +75,16 @@ public class UserService {
         // Header 로 accessToken 반환
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER, accessToken);
 
+        // Cache-Control 헤더 설정
+        String cacheControlHeader = "no-cache, no-store";
+        if (!accessToken.isEmpty()) {
+            cacheControlHeader += ", max-age=0"; // accessToken에 대해서는 캐시를 사용하지 않음
+        }
+        if (!refreshToken.isEmpty()) {
+            cacheControlHeader += ", max-age=" + 7 * 24 * 60 * 60; // refreshToken에 대해서는 캐시를 1시간 동안 유지
+        }
+        response.setHeader("Cache-Control", cacheControlHeader);
+
         // Cookie 로 refreshToken 반환
         ResponseCookie cookie = ResponseCookie.from("RefreshToken", refreshToken)
                 .maxAge(7 * 24 * 60 * 60)
