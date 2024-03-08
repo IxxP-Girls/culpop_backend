bc0014ccbb80ab4e08983c825085a165da940553
diddnwjd <yang965273@gmail.com>
2024-03-08 23:54:35 +0900

    #13 Feat: 조회수 추가

--- src/main/java/com/ixxp/culpop/dto/post/PostResponse.java
+++ src/main/java/com/ixxp/culpop/dto/post/PostResponse.java
@@ -13,14 +13,16 @@ public class PostResponse {
     private String username;
     private String title;
     private String cateName;
+    private int viewCount;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDateTime createdAt;
 
-    public PostResponse(int postId, String username, String title, String cateName, LocalDateTime createdAt) {
+    public PostResponse(int postId, String username, String title, String cateName, int viewCount, LocalDateTime createdAt) {
         this.postId = postId;
         this.username = username;
         this.title = title;
         this.cateName = cateName;
+        this.viewCount = viewCount;
         this.createdAt = createdAt;
     }
 }
