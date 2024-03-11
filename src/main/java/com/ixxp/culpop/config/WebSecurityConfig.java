d66fc00f9b861553d7e25d6f55d8204e61c90cb8
diddnwjd <yang965273@gmail.com>
2024-03-11 23:47:02 +0900

    Fix: 프론트 로컬 주소 https로 변경

--- src/main/java/com/ixxp/culpop/config/WebSecurityConfig.java
+++ src/main/java/com/ixxp/culpop/config/WebSecurityConfig.java
@@ -62,7 +62,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD")
-                .allowedOrigins("http://localhost:8080", "http://localhost:5173/", "http://43.202.13.38:8081/", "https://www.culpop.shop/", "https://my.culpop.shop/", "https://a.culpop.vercel.app/")
+                .allowedOrigins("http://localhost:8080", "https://localhost:5173/", "http://43.202.13.38:8081/", "https://www.culpop.shop/", "https://my.culpop.shop/", "https://a.culpop.vercel.app/")
                 .allowCredentials(true);
     }
 }
